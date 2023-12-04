68c8dcfc3ff21869387c67cba16b2edf09e5a9b8
Armine Simonyan <arnology.armine@gmail.com>
2023-12-04 22:38:26 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>{{ counter }}</h3>
+  <button @click="addCount()">Add 1</button>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addCount() {
+      this.$store.state.counter++;
+    },
+  },
+};
+</script>
